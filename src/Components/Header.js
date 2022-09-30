5a293de549c6e362d7273c2b541d51c4cc0a0b4c
VishalGill <vgill96@ymail.com>
2022-09-30 15:19:03 -0400

    added Weather & Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+   
+		<div>
+			<form>
+				<input placeholder='Enter City Name' />
+			</form>
+		</div>
+
+
+        
+    );
+}
+
+export default Header;
---
+++ src/Components/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Weather(props) {
+    return (
+        <div>
+            <>
+			<h1>Name of City</h1>
+			<h2>Degrees</h2>
+			<h2>Type of Day</h2>
+		</>
+        </div>
+    );
+}
+
+export default Weather;
+
+
+
