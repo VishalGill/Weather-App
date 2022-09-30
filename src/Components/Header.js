608eea712b2a072d0b1706bd9dd549152db0793c
VishalGill <vgill96@ymail.com>
2022-09-30 15:21:12 -0400

    added search button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 function Header(props) {
-    return (
-   
+	return (
 		<div>
 			<form>
-				<input placeholder='Enter City Name' />
+				<input placeholder='Enter City Name' /> <button>Search</button>
 			</form>
 		</div>
-
-
-        
-    );
+	);
 }
 
 export default Header;
